44f0cf01226499068f2f9f297a81b24acfd3b86e
ziponia <jihoon@deltaworks.co.kr>
2019-05-17 23:14:54 +0900

    2019. 05. 17. 변경사항
    - Spring boot 를 버전업 하였습니다.
    - 애플리케이션 이 더 이상 mysql 을 필요로 하지 않습니다. ( h2을 사용합니다. )

--- api/src/main/java/com/preeplus/api/conf/SecurityConfig.java
+++ api/src/main/java/com/preeplus/api/conf/SecurityConfig.java
@@ -5,21 +5,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HeaderHttpSessionStrategy;
 
 @Configuration
-@EnableWebSecurity(debug = true)
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("test").password(passwordEncoder().encode("1234")).authorities("ROLE_ADMIN", "ROLE_USER");
+		auth.inMemoryAuthentication().withUser("test").password(passwordEncoder().encode("1234")).roles("ADMIN", "USER");
+	}
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
 	}
 
 	@Override
